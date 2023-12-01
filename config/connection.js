3fd8a7b53d33dbf2970b07de994f9d374cec9fd4
Mohammad <mohammad@Mohammads-MacBook-Pro-4.local>
2023-11-30 16:51:58 -0800

    mysql

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize("newsfeed_db", "root", "Superman@70", {
+  : new Sequelize("Sac_State_Database", "root", "Superman@70", {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
